dd969dd2dca832bd0b88f0d5a8362bfc68e8ee1a
fasantos <filomenosantos@gmail.com>
2015-05-13 00:45:28 +0200

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
+#!/usr/bin/env node
+
 var express = require('express');
 var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.listen(port,function() {
+	console.log('Listening on port %d\nPress ctrl-c to quit.', port);
+});
+
 app.use(express.static(__dirname + '/app'));
-app.listen(process.env.PORT || 3000);
+
+var router = express.Router();
+
+app.get('/todos', function(req, res) {
+	setTimeout(function() {
+		res.json(todos);	
+	}, 500);
+	
+});
+
+var todos= [
+		{
+			name: 'Do production app',
+			completed: false
+		},
+		{
+			name: 'Do training app',
+			completed: true
+		},
+		{
+			name: 'Learn router-ui',
+			completed: true
+		},
+		{
+			name: 'Learn angularjs',
+			completed: true
+		}
+	];
--- app/app.js
+++ app/app.js
@@ -1,12 +1,61 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+
+var app = angular.module('todo', []);
+
+app.directive('ngBlur', function() {
+	return function(scope, elem, attrs) {
+		elem.bind('blur', function() {
+			scope.$apply(attrs.ngBlur);
+		});
+	};
+});
+
+app.controller('TodoCtrl', function ($scope, filterFilter, $http, $location) {
+	$scope.todos= [];
+	$scope.placeholder = 'Loading...';
+	$scope.statusFilter = {};
+
+	$http.get('/todos').success(function(data) {
+		$scope.todos = data;
+		$scope.placeholder = 'Add new task.'
+	});
+
+	$scope.$watch('todos', function() {
+		$scope.remaining = filterFilter($scope.todos, {completed: false}).length;
+		$scope.allchecked = ($scope.remaining > 0)? true : false;
+	}, true);
+
+	if ($location.path() == '') {$location.path('/')};
+	$scope.location = $location;
+	$scope.$watch('location.path()', function(path) {
+		$scope.statusFilter = 
+			(path == '/active')? {completed: false} : 
+			(path == '/done')? {completed: true} : 
+			null;
+
+	});
+
+	$scope.removeTodo = function(index) {
+		$scope.todos.splice(index, 1);
+	};
+
+	$scope.addTodo = function() {
+		$scope.todos.push({
+			name: $scope.newtodo,
+			completed: false
+		});
+		$scope.newtodo = '';
+	};
+
+	$scope.editTodo = function(todo) {
+		todo.editing = false;
+	};
+
+	$scope.checkAllTodo = function(allchecked) {
+		$scope.todos.forEach(function(todo) {
+			todo.completed = allchecked;
+		});
+	};
+});
